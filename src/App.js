0e4cf7b043f463be3dfcfca1ee649f80c8560777
Habip Hakkioglu <h.hakkioglu@icloud.com>
2023-08-31 19:43:04 +0200

    initial files added, json server created

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+
+//! önemli not: projeye json server eklemede zorluk yasadim. npm install komutunun öncesine sudo yazip sifreyi girerek bu sorunu astim. 
+//? Yani  sudo npm install -g json-server dedim. 
+//? sonra json-server --watch api/db.json dedim.
+//Bundan sonra mesela 3000 portunda json server calisirken baska bir terminalde projeyi baslatiyorsun ve 3001 portunda baslatiyor.
+
+
+
+function App() {
+  return (
+    <div className="App">
+       hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Courses.js
@@ -0,0 +1,7 @@
+function Courses () {
+    return <div>
+        
+    </div>;
+}
+
+export default Courses;
